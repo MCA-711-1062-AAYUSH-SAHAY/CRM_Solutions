843f60670131d64f21fb1acbbff18c16700a44d3
MCA-711-1062-AAYUSH-SAHAY <aayush.mcastudent24.1062@cimage.in>
2026-08-11 14:48:48 +0530

    dummy server added to make the folders for clients

---
+++ examples/day1_learning/app_demo/crm_server.py
@@ -0,0 +1,18 @@
+from flask import Flask,jsonify
+from pathlib import Path
+
+app = Flask(__name__)
+
+CRM_ROOT = Path.home() / "crm_root"
+
+@app.route("/make_dir/<client_id>",methods=["GET"])
+def make_dir(client_id):
+    folder=CRM_ROOT/client_id
+    folder.mkdir(parents=True,exist_ok=True)
+
+    return jsonify({
+        "Success":"True",
+        "folder": str(folder)
+    })
+
+app.run(host:"0.0.0.0",port=5000)
